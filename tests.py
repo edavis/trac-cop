321b11958c8b8e5dec1411231b9bf1267641d2ae
Eric Davis <ed@npri.org>
2012-08-08 16:19:00 -0700

    Update tests to use 'Delivered-To'

--- tests.py
+++ tests.py
@@ -3,8 +3,8 @@ import email
 import trac_cop
 
 def test_get_ticket_id():
-    msg = email.message_from_string('To: trac+1@transparentnevada.com')
-    eq_(1, trac_cop.get_ticket_id(msg))
+    eq_(1, trac_cop.get_ticket_id('trac+1@transparentnevada.com'))
+    eq_(10, trac_cop.get_ticket_id('trac+10@transparentnevada.com'))
 
 def test_get_author():
     msg = email.message_from_string("To: trac+1@transparentnevada.com\n\nBy: Kristi Boosman\n\nmessage content here")
@@ -16,7 +16,7 @@ def test_get_author():
     msg = email.message_from_string("To: trac+1@transparentnevada.com\n\nmessage content here")
     eq_("(Unknown)", trac_cop.get_author(msg))
 
-    msg = email.message_from_string("Bcc: trac+1@transparentnevada.com\n\nmessage content here")
+    msg = email.message_from_string("Delivered-To: trac+1@transparentnevada.com\n\nmessage content here")
     eq_("Eric Davis", trac_cop.get_author(msg))
 
 def test_get_comment():
@@ -29,7 +29,7 @@ message content here
 hello world""")
     eq_("message content here\n\nhello world", trac_cop.get_comment(msg))
 
-    msg = email.message_from_string("""Bcc: trac+1@transparentnevada.com
+    msg = email.message_from_string("""Delivered-To: trac+1@transparentnevada.com
 
 hello world""")
     eq_("hello world", trac_cop.get_comment(msg))
