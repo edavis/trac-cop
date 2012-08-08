3a0c5fe5a8d2fcdb3d0fbd98e916853251f3940c
Eric Davis <ed@npri.org>
2012-08-08 13:38:51 -0700

    Add argparse as a requirement

--- setup.py
+++ setup.py
@@ -3,6 +3,13 @@ try:
 except ImportError:
     from distutils.core import setup
 
+install_requires = ["Trac==0.12.3"]
+
+try:
+    import argparse
+except ImportError:
+    install_requires.append("argparse")
+
 setup(
     name             = "trac-cop",
     version          = "0.1",
@@ -11,7 +18,7 @@ setup(
     author_email     = "ed@npri.org",
     url              = "https://github.com/edavis/trac-cop",
     py_modules       = ["trac_cop"],
-    install_requires = ["Trac==0.12.3"],
+    install_requires = install_requires,
     entry_points     = {
         "console_scripts": [
             "cop.py=trac_cop:main",
